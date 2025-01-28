1e346647f2d42ed5ea68c264b3e7d6f64efae054
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-28 12:26:06 +0300

    Добавьте файлы проекта.

---
+++ Books/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace Books
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListBoxBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DataGridBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
