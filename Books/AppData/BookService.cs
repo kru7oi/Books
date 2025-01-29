8967ed7a0a50c96df72f2c934cf86350296212cf
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-29 13:09:47 +0300

    Изменены файлы проекта.

---
+++ Books/AppData/BookService.cs
@@ -0,0 +1,27 @@
+﻿using Books.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Books.AppData
+{
+    public class BookService
+    {
+        public const string JSON_PATH = "https://mysafeinfo.com/api/data?list=bestnovels&format=json&case=default";
+
+        public List<Book> Books { get; private set; }
+
+        public async Task LoadBookAsync()
+        {
+            try
+            {
+                HttpClient client = new();
+                var response = await client.GetAsync(JSON_PATH);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ Books/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace Books.Model
+{
+    public class Book
+    {
+        public int Rank { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Published { get; set; }
+        public string Country { get; set; }
+        public string OriginalLanguage { get; set; }
+        public string PageRange { get; set; }
+        public string WordCount { get; set; }
+        public int ID { get; set; }
+    }
+}
