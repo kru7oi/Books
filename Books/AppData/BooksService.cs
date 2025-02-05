0bcf371b6afdc5edbe8b69f2231b83ddde86980c
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-02-05 13:22:53 +0300

    Добавлены файлы проекта.

--- Books/AppData/BooksService.cs
+++ Books/AppData/BooksService.cs
@@ -1,7 +1,27 @@
-﻿namespace Books.AppData
+﻿using Books.Model;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Books.AppData
 {
     public class BooksService
     {
         private const string JSON_PATH = "https://mysafeinfo.com/api/data?list=bestnovels&format=json&case=default";
+
+        public async Task<List<Book>> GetBooksAsync()
+        {
+
+            // 1. Создаем НТТP - клиент (делает запрос на сервер и получает ответ)
+            HttpClient httpClient = new HttpClient();
+
+            // 2. Создаем переменную для хранения ответа от сервера.
+            // HTTP - клиент просит сервер получить строку из JSON - файла, который указан в скобках
+            var response = await httpClient.GetStringAsync(JSON_PATH);
+
+            // 3. Возвращаем список 
+            return JsonConvert.DeserializeObject<List<Book>>(response);
+        }
     }
 }
